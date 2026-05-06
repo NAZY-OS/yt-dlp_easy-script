34b89a52260e8487d2306e3e4f65670b49c453e3
NAZY-OS ~ OFFICIAL <114874290+NAZY-OS@users.noreply.github.com>
2026-05-06 10:43:44 +0200

    Initial commit

---
+++ ffmpeg_scripts/wav_to_mp3.sh
@@ -0,0 +1,86 @@
+#!/bin/bash
+
+# MIT License
+# 
+# Copyright (c) 2026 NAZY-OS
+# 
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+
+# Author: NAZY-OS
+# Version: 1.0.1
+
+SCRIPTPATH="$(dirname -- "${BASH_SOURCE[0]}")"
+
+show_help() {
+    echo "Usage: $0 -i <input_file> -o <output_file> [-p <output_directory>]"
+    echo "Options:"
+    echo "    -i, --input     Input WAV audio file."
+    echo "    -o, --output    Output MP3 audio file name."
+    echo "    -p, --path      (Optional) Output directory. Default is current directory."
+    echo "    -h, --help      Show this help message."
+}
+
+# Using getopt to parse input
+OPTIONS=$(getopt -o i:o:p:h --long input:,output:,path:,help -- "$@")
+if [ $? -ne 0 ]; then
+    show_help
+    exit 1
+fi
+
+eval set -- "$OPTIONS"
+
+# Default values
+OUTPUT_PATH="."
+
+# Parse options
+while true; do
+    case "$1" in
+        -i | --input)
+            INPUT="$2"
+            shift 2
+            ;;
+        -o | --output)
+            OUTPUT="$2"
+            shift 2
+            ;;
+        -p | --path)
+            OUTPUT_PATH="$2"
+            shift 2
+            ;;
+        -h | --help)
+            show_help
+            exit 0
+            ;;
+        --) 
+            shift
+            break
+            ;;
+        *) 
+            echo "Invalid option"
+            exit 1
+            ;;
+    esac
+done
+
+# Check required parameters
+if [ -z "$INPUT" ] || [ -z "$OUTPUT" ]; then
+    echo "Missing required parameters."
+    show_help
+    exit 1
+fi
+
+# Ensure the output filename has the .mp3 extension
+if [[ "$OUTPUT" != *.mp3 ]]; then
+    OUTPUT="$OUTPUT.mp3"
+fi
+
+# Execute ffmpeg command
+ffmpeg -i "$INPUT" "$OUTPUT_PATH/$OUTPUT"
+
+echo "Audio converted successfully from $INPUT to $OUTPUT_PATH/$OUTPUT"
